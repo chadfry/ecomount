bbd958cf40658cd92a8b4eaade8ce9a169f8305f
Chad <chadfry@me.com>
2015-05-10 16:05:52 -0700

    automated grunt for concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,17 @@ module.exports = function(grunt) {
           dest: 'dist/css/built.css',
         },
       },
+    watch: {
+        files: ['css/bootstrap.css','style.css','onepage.css','css/dark.css','css/et-line.css','css/animate.css','css/magnific-popup.css','css/fonts.css','css/responsive.css'],
+        tasks: 'concat',
+
+    }
+
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 
 };
